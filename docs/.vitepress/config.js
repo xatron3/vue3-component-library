ecbb1ad1e885cdcc912b2b8748d258f36eeb219c
xatz <xatron3@gmail.com>
2022-10-31 17:09:55 +0100

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "Vue3 Components Library",
+  base: "vue-components",
+  themeConfig: {
+    sidebar: [
+      {
+        text: "Components",
+        items: [
+          {
+            text: "Input Text",
+            link: "/components/input-text",
+          },
+        ],
+      },
+    ],
+    nav: [{ text: "Home", link: "/" }],
+  },
+};
---
+++ docs/components/input-text.md
@@ -0,0 +1,9 @@
+# input-text
+
+InputText is a cool component. Here's how to use it...
+
+```html
+<template>
+  <input-text />
+</template>
+```
